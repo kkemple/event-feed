d68d2d9e61cc746feb1f5d3da2db595f293b0bf9
Kurtis Kemple <kurtiskemple@gmail.com>
2016-09-02 21:02:00 -0400

    allow ORM to create tables if missing from database

--- src/server/plugins/orm/index.js
+++ src/server/plugins/orm/index.js
@@ -1,3 +1,4 @@
+import debug from 'debug'
 import r from 'rethinkdb'
 
 import Events from './events'
@@ -20,6 +21,15 @@ type HapiServer = {
   statsd: Object
 }
 
+type Logger = (s: string, ...a: any) => void
+
+const logger: Logger = debug('server:orm')
+
+const TABLES: Array<string> = [
+  'events',
+  'settings'
+]
+
 const plugin = {
   async register (
     server: HapiServer,
@@ -32,12 +42,33 @@ const plugin = {
     },
     next: (e?: Error) => void
   ) {
+    logger('[register] setting up database...')
+
     try {
       // get a connection to rethink
       const connection: Connection = await r.connect(options)
 
       // this allows us to do r.table() instead of r.db().table() in models
       connection.use(options.db)
+      logger('[register] database connection established...')
+
+      const tables: Array<string> = await r.tableList().run(connection)
+      logger('[register] found `%s` tables in database...', tables.join(', '))
+
+      // create the tables we need if they don't exist
+      await Promise.all(TABLES.map(async T => {
+        if (!~tables.indexOf(T)) {
+          logger('[register] creating table `%s`...', T)
+
+          await r.tableCreate(T).run(connection)
+          await r.indexCreate('id').run(connection)
+
+          logger('[register] table `%s` created...')
+          return Promise.resolve()
+        }
+
+        return Promise.resolve
+      }))
 
       server.expose('events', new Events(connection, server.statsd))
       server.expose('settings', new Settings(connection, server.statsd))
